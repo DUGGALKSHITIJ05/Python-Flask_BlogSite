0209eac76938177c78e548e3b2930384e7d4f506
Gautam <gautammaini9@gmail.com>
2020-12-03 21:07:06 +0530

    Init

---
+++ app/auth/forms.py
@@ -0,0 +1,37 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, SubmitField, PasswordField,TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError,Length
+from app.models import User
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators = [DataRequired()])
+    password = PasswordField('Password', validators = [DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators = [DataRequired()])
+    email = StringField('Email', validators =[DataRequired(), Email()])
+    password = PasswordField('Password', validators =[DataRequired()])
+    password2 = PasswordField('Repeat password', validators =[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def register_username(self, username):
+        user = User.query.filter_by(username = username.data).first()
+        if user is not None:
+            raise ValidationError('Please use Different Username.')
+
+    def register_email(self, email):
+        user = User.query.filter_by(email = email.data).first()
+        if user is not None:
+            raise ValidationError('Please use Different Email Address.')
+
+class ResetPasswordRequestForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Request Password Reset')
---
+++ microblog.py
@@ -0,0 +1,9 @@
+from flask import current_app
+from app.models import User, Post, Notification, Message
+from app import create_app, db
+
+app = create_app()
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post, 'Message': Message, 'Notification': Notification}
+    
